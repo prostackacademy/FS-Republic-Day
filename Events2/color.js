4e27f110878f5d9c57b46e3a59cf0d542d995126
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-03-14 18:57:16 +0530

    okie

---
+++ Events2/color.js
@@ -0,0 +1,16 @@
+function change_Color_Mourseover() {
+    document.getElementById('one').style.backgroundColor = "green"
+}
+function change_Color_Mourseout() {
+    document.getElementById('two').style.backgroundColor = "yellow"
+}
+function change_Color_Click() {
+    document.getElementById('three').style.backgroundColor = "red"
+}
+function change_Color_DblClick() {
+    document.getElementById('four').style.backgroundColor = "blue"
+}
+
+function change_Button_Name() {
+    document.getElementById('five').innerHTML = "Nayana"
+}
